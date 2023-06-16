722887a9cd45169a2ea90d3e67797f9292e55f76
Austin.Nguyen <Austin.Nguyen@psionline.com>
2023-06-16 12:25:54 -0500

    Added dependencies

--- GameApp/Database/dbo/Tables/Game.sql
+++ GameApp/Database/dbo/Tables/Game.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Game]
 (
-	[Id] INT NOT NULL , 
     [GameId] INT NOT NULL, 
     [GameName] NVARCHAR(255) NULL, 
     [DevId] INT NULL, 
+    [GameDescription] NVARCHAR(255) NULL, 
     PRIMARY KEY ([GameId]), 
     CONSTRAINT [FK_Game_Dev] FOREIGN KEY (DevId) REFERENCES DevCompany(DevCoId)
 )
