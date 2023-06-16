98edc96a83e1328fb785d31c9da65a8dde156906
Austin.Nguyen <Austin.Nguyen@psionline.com>
2023-06-16 10:21:27 -0500

    Created Database and initial project layout

---
+++ GameApp/Database/dbo/Tables/DevCompany.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DevCompany]
+(
+    [DevCoId] INT NOT NULL, 
+    [DevName] NVARCHAR(255) NULL, 
+    [DevAddress] NVARCHAR(255) NULL, 
+    PRIMARY KEY ([DevCoId])
+)
---
+++ GameApp/Database/dbo/Tables/Game.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Game]
+(
+	[Id] INT NOT NULL , 
+    [GameId] INT NOT NULL, 
+    [GameName] NVARCHAR(255) NULL, 
+    [DevId] INT NULL, 
+    PRIMARY KEY ([GameId]), 
+    CONSTRAINT [FK_Game_Dev] FOREIGN KEY (DevId) REFERENCES DevCompany(DevCoId)
+)
