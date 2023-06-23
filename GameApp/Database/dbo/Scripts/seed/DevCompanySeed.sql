56d34e4785033e83fca73071773376128b134f8b
Austin.Nguyen <Austin.Nguyen@psionline.com>
2023-06-23 14:19:42 -0500

    Added db seed and settings to turn off cache

---
+++ GameApp/Database/dbo/Scripts/seed/DevCompanySeed.sql
@@ -0,0 +1,8 @@
+﻿IF NOT EXISTS (SELECT 1 FROM [dbo].[DevCompany])
+BEGIN
+	INSERT INTO [dbo].[DevCompany] ([DevName], [DevAddress], [FoundedDate], [MostPopularGame])
+	VALUES
+		('Rockstar Games', 'New York, NY', '1998-12-21', 'Grand Theft Auto'),
+		('Nintendo', 'Kyoto, Japan', '1889-09-23', 'Mario'),
+		('Bungie Inc.', 'Bellevue, WA', '1991-05-08', 'Halo')
+END
